f29b04c0109431e9a8029c9348335d67f20fbff9
Laura Outhuis <outhuislaura@gmail.com>
2022-11-30 21:33:06 +0100

    intial start

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,71 @@
+"""This file is where you will define any models for
+your web application, where each model represents
+some type of data you want to store in your database."""
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+"""Remember that each time you change anything in auctions/models.py
+you'll need to first run python3 manage.py makemigrations and then
+python manage.py migrate to migrate those changes to your database"""
+
+""" The application should have at least three models in addition to the User
+model: auction listings, bids and comments."""
 
 
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    """Class to the define the category a listing belongs to """
+    category_type = models.CharField(max_length=64)
+
+    def __str__(self):
+        """ This function makes sure that in the admin panel the
+        name of the category is displayed, rather than 'Category
+        object(1), etc."""
+        return self.category_type
+
+
+class AuctionListings(models.Model):
+    """ Pass in everything a listing needs"""
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=250)
+    image_url = models.CharField(max_length=1000)
+    price = models.FloatField(default=0.0)
+    active_item = models.BooleanField()
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, blank=True,
+        null=True, related_name="user")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    # owner = models.ForeignKey(User, on_delete=models.CASCADE,
+    # blank=True, null=True, related_name="user")
+
+    def __str__(self):
+        """ This function makes sure that in the admin panel the
+        name of the category is displayed, rather than 'Category
+        object(1), etc."""
+        return self.title
+
+
+class Bids(models.Model):
+    pass
+
+
+class Comments(models.Model):
+    pass
+
+
+""" Personal notes
+  ???? why use models.Model in the class parameters?
+
+blank=True -->  One can also add more built-in field validations
+                for applying or removing certain constraints on a particular
+                field. blank=True will make the field accept blank values.
+                It simply means that field can be empty.
+
+on_delete=models.CASCADE -->    if the user gets deleted, the listing gets
+                                deleted as well
+
+  """
--- auctions/views.py
+++ auctions/views.py
@@ -1,14 +1,20 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
+# from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Category, AuctionListings
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    # Check in models.py if an listing (in class AuctionListings is active)
+    # proberen of iets als if active_listing == True ook kan
+    active_listing = AuctionListings.objects.filter(active_item=True)
+    return render(request, "auctions/index.html", {
+        "listings": active_listing
+    })
 
 
 def login_view(request):
@@ -61,3 +67,42 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def create_listing(request):
+    if request.method == "GET":
+        # Source for listing all the categories:
+        # https://cs50.harvard.edu/web/2020/notes/4/ (Chapter Shell)
+        all_categories = Category.objects.all()
+        return render(request, "auctions/create_listing.html", {
+            "categories": all_categories
+        })
+    else:
+        user = request.user
+
+        # Get the needed data from the form from create_listing.html
+        title = request.POST["title"]
+        description = request.POST["description"]
+        price = request.POST["price"]
+        image_url = request.POST["image_url"]
+        category = request.POST["category"]
+
+        category_info = Category.objects.get(category_type=category)
+
+        # Create a new lisiting object
+        new_listing = AuctionListings(
+            title=title,
+            description=description,
+            image_url=image_url,
+            price=float(price),
+            category=category_info,
+            owner=user
+        )
+
+        new_listing.save()  # See source material
+        """ waarom reverse index?"""
+        return HttpResponseRedirect(reverse(index))
+
+
+def categories(request):
+    return render(request, "auctions/categories.html")
